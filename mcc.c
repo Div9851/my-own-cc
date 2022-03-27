578ef5d20ee63b8a5903cb14fc53b5d2468ea644
Satoshi Warita <div9851.tech@gmail.com>
2022-03-27 17:09:57 +0900

    <,<=,>,>=を追加

--- mcc.c
+++ mcc.c
@@ -109,13 +109,14 @@ Token *tokenize() {
             continue;
         }
 
-        if (startswith(p, "==") || startswith(p, "!=")) {
+        if (startswith(p, "==") || startswith(p, "!=") || startswith(p, "<=") ||
+            startswith(p, ">=")) {
             cur = new_token(TK_RESERVED, cur, p, 2);
             p += 2;
             continue;
         }
 
-        if (strchr("+-*/()", *p)) {
+        if (strchr("+-*/()<>", *p)) {
             cur = new_token(TK_RESERVED, cur, p++, 1);
             continue;
         }
@@ -142,6 +143,8 @@ typedef enum {
     ND_DIV, // /
     ND_EQ,  // ==
     ND_NE,  // !=
+    ND_LT,  // <
+    ND_LE,  // <=
     ND_NUM, // 整数
 } NodeKind;
 
@@ -172,6 +175,7 @@ Node *new_node_num(int val) {
 
 Node *expr();
 Node *equality();
+Node *relational();
 Node *add();
 Node *mul();
 Node *unary();
@@ -180,13 +184,30 @@ Node *primary();
 Node *expr() { return equality(); }
 
 Node *equality() {
-    Node *node = add();
+    Node *node = relational();
 
     for (;;) {
         if (consume("=="))
-            node = new_node(ND_EQ, node, add());
+            node = new_node(ND_EQ, node, relational());
         else if (consume("!="))
-            node = new_node(ND_NE, node, add());
+            node = new_node(ND_NE, node, relational());
+        else
+            return node;
+    }
+}
+
+Node *relational() {
+    Node *node = add();
+
+    for (;;) {
+        if (consume("<")) {
+            node = new_node(ND_LT, node, add());
+        } else if (consume("<="))
+            node = new_node(ND_LE, node, add());
+        else if (consume(">")) {
+            node = new_node(ND_LT, add(), node);
+        } else if (consume(">="))
+            node = new_node(ND_LE, add(), node);
         else
             return node;
     }
@@ -272,6 +293,16 @@ void gen(Node *node) {
     case ND_NE:
         printf("    cmp rax, rdi\n");
         printf("    setne al\n");
+        printf("    movzb rax, al\n");
+        break;
+    case ND_LT:
+        printf("    cmp rax, rdi\n");
+        printf("    setl al\n");
+        printf("    movzb rax, al\n");
+        break;
+    case ND_LE:
+        printf("    cmp rax, rdi\n");
+        printf("    setle al\n");
         printf("    movzb rax, al\n");
         break;
     }
